2e1bb53b4b7e53a5574a23d5fc3cf9ec1e1c14f7
Nour-Agha99 <temoagha@gmail.com>
2023-03-21 20:20:19 +0200

    update some files *HTML,css,js

--- public/DOM.js
+++ public/DOM.js
@@ -1,10 +1,13 @@
 let input = document.querySelector(".search-input input");
 let inputTop = document.querySelector(".top-section");
+let popup = document.querySelector(".autocom-box");
 
 input.addEventListener("focus", () => {
   inputTop.classList.toggle("focus");
+  popup.classList.toggle("close");
 });
 
 input.addEventListener("blur", () => {
   inputTop.classList.toggle("focus");
+  popup.classList.toggle("close");
 });
